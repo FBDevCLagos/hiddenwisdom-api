0e286ef5092b6f2f08f210c96e1bb5b35204c992
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-08-04 16:28:59 +0100

    add default usertype to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates :username, :email, :fb_id, :first_name, :last_name, presence: true
   validates :email, uniqueness: true
+
+  enum user_type: {regular: 0, moderator: 1, admin: 2}
 end
