eb0f70e8aff23cd488a8b3c7650285ceb3948cee
andela-gogbara <godwin.onisofien@andela.com>
2016-09-10 17:17:20 +0100

    feature(tag_serializer): add tag serilaizer

--- app/serializers/proverb_serializer.rb
+++ app/serializers/proverb_serializer.rb
@@ -1,5 +1,8 @@
 class ProverbSerializer < ActiveModel::Serializer
-  attributes :id, :body, :language, :status, :root_id, :created_at, :translations
+  attributes :id, :body, :language, :status, :root_id, :tags, :created_at, :translations
+
+  # has_many :taggings
+  has_many :tags
 
   def translations
     object.translations
