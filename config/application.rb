1c1aa308e673b23f75d780058206843514ace7d0
andela-gogbara <godwin.onisofien@andela.com>
2016-09-11 09:01:50 +0100

    add rack-cors gem config closes #21

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module ProverbsApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
     config.middleware.insert_before 0, "Rack::Cors" do
     allow do
         origins '*'
