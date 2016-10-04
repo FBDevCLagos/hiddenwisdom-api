2a16e06b60364285f472bcb23f9ba04ad57b3001
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2016-10-04 16:47:26 +0100

    add select before join in the product search scope

--- app/models/proverb.rb
+++ app/models/proverb.rb
@@ -26,7 +26,7 @@ class Proverb < ActiveRecord::Base
     language = params[:language].downcase if params[:language]
     ord = params["random"] ? "RANDOM()" : "id #{params[:direction]}"
     set_order = ord == "id " ? "proverbs.id desc" : ord
-    Proverb.joins(:tags).where(
+    Proverb.select("proverbs.*, #{ord}").joins(:tags).where(
       "tags.name LIKE ? and lower(language) LIKE ?",
       "%#{tag}%",
       "%#{language}%"
