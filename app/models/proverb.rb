189ed94d2b44e172c3aa8dcaba37479912d1dc68
Osmond Oscar <osmond.oranagwa@andela.com>
2016-10-09 18:19:12 +0100

    refactor search implementation

--- app/models/proverb.rb
+++ app/models/proverb.rb
@@ -21,23 +21,39 @@ class Proverb < ActiveRecord::Base
     self.tags.map(&:name).join(", ")
   end
 
-  scope :search, lambda { |params = {}|
-    tag = params[:tag].downcase if params[:tag]
-    language = params[:language].downcase if params[:language]
-    ord = params["random"] ? "RANDOM()" : "id #{params[:direction]}"
-    set_order = ord == "id " ? "proverbs.id desc" : ord
-    Proverb.select("proverbs.*, #{ord}").joins(:tags).where(
-      "tags.name LIKE ? and lower(language) LIKE ?",
-      "%#{tag}%",
-      "%#{language}%"
-    ).order(set_order).uniq
-  }
-
   def self.paginate(params)
-    limit = params[:limit] ? params[:limit] : 20
-    page = params[:page] ? params[:page] : 0
-    # offset = limit.to_i * (page.to_i - 1)
-    offset = page == 0 ? 0 : (page.to_i - 1) * limit.to_i
-    search(params).limit(limit).offset(offset)
+    params[:direction] = "RANDOM()" if params[:direction] == 'random'
+    query = self.includes(:tags)
+    query = query.filter_language(params) if params[:language]
+    query = query.filter_tag(params) if params[:tag]
+    query.filter_order(params).filter_limit(params).filter_offset(params)
   end
+
+  scope :filter_language, ->(args) { where(language: args[:language])}
+  scope :filter_tag, -> (args) { where(tags: {name: args[:tag]})}
+  scope :filter_order, -> (args) { order("proverbs.#{args[:order] || 'id'} #{args[:direction] || 'desc'} ")}
+  scope :filter_limit, -> (args) { limit(args[:limit] || 20)}
+  scope :filter_offset, -> (args) { offset(args[:offset] || 0)}
+
+
+  # scope :search, lambda { |params = {}|
+  #   tag = params[:tag].downcase if params[:tag]
+  #   language = params[:language].downcase if params[:language]
+  #   ord = params["random"] ? "RANDOM()" : "id #{params[:direction]}"
+  #   set_order = ord == "id " ? "proverbs.id desc" : ord
+  #   Proverb.select("proverbs.*, #{ord}").joins(:tags).where(
+  #     "tags.name LIKE ? and lower(language) LIKE ?",
+  #     "%#{tag}%",
+  #     "%#{language}%"
+  #   ).order(set_order).uniq
+  # }
+  #
+  # def self.paginate(params)
+  #   binding.pry
+  #   limit = params[:limit] ? params[:limit] : 20
+  #   page = params[:page] ? params[:page] : 0
+  #   # offset = limit.to_i * (page.to_i - 1)
+  #   offset = page == 0 ? 0 : (page.to_i - 1) * limit.to_i
+  #   search(params).limit(limit).offset(offset)
+  # end
 end
